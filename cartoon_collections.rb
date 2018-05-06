036ec8c057d4d4d7c8dbac70130051fd3c48b592
Razibul Ahmed <razibul.ahmed@gmail.com>
2018-05-06 19:14:34 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,8 +10,9 @@ def summon_captain_planet(array) # code an argument here
   
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  return array.any? {|word| word.length > 4}
 end
 
 def find_the_cheese# code an argument here
